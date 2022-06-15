fb20999743148ac07b7523ce4faeaa8506deb66c
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2022-06-15 22:33:56 +0200

    add gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -79,6 +79,9 @@ return packer.startup(function(use)
   use "p00f/nvim-ts-rainbow"
   use "JoosepAlviste/nvim-ts-context-commentstring"
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
     require("packer").sync()
