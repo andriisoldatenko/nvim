917c58de512ba4a4eed9d3d166e3d43f46bf866d
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2022-06-23 19:51:16 +0200

    add nvim-treesitter-textobjects

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -97,6 +97,7 @@ return packer.startup(function(use)
 	})
 	use("p00f/nvim-ts-rainbow")
 	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use("nvim-treesitter/nvim-treesitter-textobjects")
 
 	-- Git
 	use("lewis6991/gitsigns.nvim")
