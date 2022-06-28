7f303b3812127ed7ba3f9931a68376b866b47937
Andrii Soldatenko <andrii.soldatenko@sentry.io>
2022-06-28 16:06:51 +0200

    use solarized-flat

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,9 @@
-local colorscheme = "solarized"
+local colorscheme = "solarized-flat"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	vim.notify("colorscheme " .. colorscheme .. " not found!")
 	return
 end
+
+vim.g.solarized_statusline = "flat"
